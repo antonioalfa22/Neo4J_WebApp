4b3d964c426c49c33237fe9e561cde6304a4c774
Antonio Paya <antonioalfa22@gmail.com>
2017-10-24 22:20:19 +0200

    Cambiados algunos aspectos

--- src/neo4jApi.js
+++ src/neo4jApi.js
@@ -6,7 +6,7 @@ var Influencias = require('./models/Influencias');
 var _ = require('lodash');
 
 var neo4j = window.neo4j.v1;
-var driver = neo4j.driver("bolt://localhost", neo4j.auth.basic("neo4j", "defensa4"));
+var driver = neo4j.driver("bolt://localhost", neo4j.auth.basic("neo4j", "1234"));
 
 /**
  * Metodo que busca lenguajes en la base de datos
